efbfce5f6987f351f8b82422ed2950342bbd3ab7
abel <vnponce8@gmail.com>
2023-12-22 19:22:17 -0600

    refactor(shortener): remove not needed function

--- libs/url.py
+++ libs/url.py
@@ -2,11 +2,6 @@ import hashlib
 
 
 def generate_url(url: str, size: int = 7, starting_index: int = 0) -> str:
-    generated = to_shortcode(url, size, starting_index)
-    return generated
-
-
-def to_shortcode(url: str, size: int = 7, starting_index: int = 0) -> str:
     result = hashlib.md5(url.encode())
     final_index = starting_index + size
     return result.hexdigest()[starting_index:final_index]
